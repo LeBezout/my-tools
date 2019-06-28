b2be6d83eacec14935febe5e0768f03db6a42db8
Bezout <lebezout@gmail.com>
2019-06-28 09:48:08 +0200

    Update assert-all-converter.sh

--- tools/assert-all-converter.sh
+++ tools/assert-all-converter.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 # Script de conversion d'un bloc de lignes Assertions.assert* vers un bloc Assertions.assertAll pour JUnit5
+# Utilisation :
+#  - ouvrir un fichier texte (ex: ~/data.txt)
+#  - copier-coller le bloc de code JUnit contenant toutes les lignes d'assertions
+#  - sauvegarder le fichier puis lancer ce script avec ce fichier en paramètre
+#  - ré-ouvrir (re-charger) le fichier et récupérer le résultat et le coller dans l'IDE
 
-tmpfile=${1:-~/data.txt}
+# fichier à traiter, soit le fichier passé en paramètre soit un fichier nommé data.txt dans le dossier home de l'utilisateur
+readonly datafile=${1:-~/data.txt}
 
 # TODO : handle comments //
 
 # Supprimer la dernière virgule
-sed -i "$ s/.$//" "${tmpfile}"
+sed -i "$ s/.$//" "${datafile}"
 
 # Remplacer tous les ; de fin de ligne par une virgule
-sed -i "s/);$/),/g" "${tmpfile}"
+sed -i "s/);$/),/g" "${datafile}"
 
 # Ajouter () -> devant chaque ligne (en ignorant les lignes vides)
-sed -i '/^[[:space:]]*$/!s/^[[:space:]]*/() -> /g' "${tmpfile}"
+sed -i '/^[[:space:]]*$/!s/^[[:space:]]*/() -> /g' "${datafile}"
 
 # Ajouter assertAll au début
-sed -i "1iAssertions.assertAll(" "${tmpfile}"
+sed -i "1iAssertions.assertAll(" "${datafile}"
 
 # Ajouter ); à la fin
-echo ");" >> "${tmpfile}"
+echo ");" >> "${datafile}"
